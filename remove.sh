5b1305f803a5fd0bcca3278d1ed31bcf95667c2f
Subhi Al Hasan <jodevsa@gmail.com>
2022-02-19 02:29:07 +0100

    Delete remove.sh

--- remove.sh
+++
@@ -1,4 +0,0 @@
-kubectl delete deployment wireguard-operator-controller-manager
-kubectl delete catalogsource.operators.coreos.com wireguard-operator-catalog
-kubectl delete subscriptions.operators.coreos.com wireguard-operator-v0-0-1-sub
-kubectl delete csv wireguard-operator.v0.0.1
