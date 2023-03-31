9e7e19a980789d65e2cb36ac34a8e4dac07e1220
Subhi Al Hasan <jodevsa@gmail.com>
2023-03-31 11:58:52 +0200

    support EgressNetworkPolicies for peers (#28)
    * support EgressNetworkPolicies for peers
    
    * new release
    
    * fix Makefile and make wireguard-operator work with go1.20.2
    
    * create the logic to convert networkPolicies to iptable-restore rules
    
    * new release
    
    * finish the logic for network policies
    
    * new release
    
    * fix tests
    
    * fix tests
    
    * new release
    
    * fix tests
    
    * new release
    
    * use uint16 for networkpolicies port value
    
    * new release
    
    * fix wireguard container to be able to restart if it was killed for any reason
    
    * clean up wireguard-image Dockerfile
    
    * new release

--- wireguard-image/entrypoint.sh
+++ wireguard-image/entrypoint.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 
 set -e
-
+IPTABLE_FILE=/tmp/wireguard/iptable
 
 function shutdown_wg() {
-  echo "Shutting down Wireguard (boringtun)"
+  echo "Shutting down Wireguard"
   wg-quick down "$1"
   exit 0
 }
 
-function start_wg() {
-  echo "Starting up Wireguard (boringtun)"
-  wg-quick up "$1"
-  infinite_loop "$1"
-}
-function setup_NAT() {
-  echo "Setting up NAT"
-  /usr/sbin/iptables-legacy -t nat -I POSTROUTING 1 -s $SUB_NET -o eth0 -j MASQUERADE
-}
 function update_config() {
+  echo "Update iptable rules"
+  if [ -f "$IPTABLE_FILE" ]; then
+    iptables-restore < "$IPTABLE_FILE"
+  fi
   echo "Updating config (hot reload)"
   cp /tmp/wireguard/config /etc/wireguard/wg0.conf  
   wg syncconf wg0 <(wg-quick strip wg0)
@@ -27,7 +22,10 @@ function update_config() {
 function watch_and_update() {
   trap 'shutdown_wg "$1"' SIGTERM SIGINT SIGQUIT
   cp /tmp/wireguard/config /etc/wireguard/wg0.conf
-  wg-quick up wg0
+  wg-quick up wg0 &
+  if [ -f "$IPTABLE_FILE" ]; then
+    iptables-restore < "$IPTABLE_FILE"
+  fi
   fswatch -o /tmp/wireguard/ | (while read; do update_config; done)
 }
 
@@ -35,5 +33,4 @@ mkdir -p /dev/net
 if [ ! -c /dev/net/tun ]; then
     mknod /dev/net/tun c 10 200
 fi
-setup_NAT
 watch_and_update  
