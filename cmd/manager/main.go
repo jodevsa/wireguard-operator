36732e70c7bec7696efa168c6b2a583c60a825dc
Subhi Al Hasan <jodevsa@gmail.com>
2023-04-29 04:23:45 +0300

    fix

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -57,7 +57,7 @@ func main() {
 	var wgImage string
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.StringVar(&wgImage, "wireguard-container-image", "", "The image used for wireguard server")
+	flag.StringVar(&wgImage, "agent-image", "", "The image used for wireguard server")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
