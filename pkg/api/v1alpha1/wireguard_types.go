c4c4de58fd469b3a530adfc2cea327cda6b3f420
Matt Beckett <matt@beckett.cloud>
2023-04-29 13:15:10 +0100

    Fix invalid reference to annotations metadata on Wireguard agent service spec

--- pkg/api/v1alpha1/wireguard_types.go
+++ pkg/api/v1alpha1/wireguard_types.go
@@ -46,7 +46,7 @@ type WireguardSpec struct {
 	// A field that specifies the type of Kubernetes service that should be used for the Wireguard VPN. This could be NodePort or LoadBalancer, depending on the needs of the deployment.
 	ServiceType corev1.ServiceType `json:"serviceType,omitempty"`
 	// A map of key value strings for service annotations
-	ServiceAnnotations metav1.Annotations `json:"serviceAnnotations,omitempty"`
+	ServiceAnnotations map[string]string `json:"serviceAnnotations,omitempty"`
 	// A boolean field that specifies whether IP forwarding should be enabled on the Wireguard VPN pod at startup. This can be useful to enable if the peers are having problems with sending traffic to the internet.
 	EnableIpForwardOnPodInit bool `json:"enableIpForwardOnPodInit,omitempty"`
 }
